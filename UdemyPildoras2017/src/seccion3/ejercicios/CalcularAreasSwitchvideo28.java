fcf8bf0e8b0d206bb58165b36cdd341ff62fb554
Jesús González <Jess Gonzlez@x0d4y>
2017-10-25 02:06:04 -0500

    Ejercicios Curso Udemy 25-10-17

--- UdemyPildoras2017/src/seccion3/ejercicios/CalcularAreasSwitchvideo28.java
+++ UdemyPildoras2017/src/seccion3/ejercicios/CalcularAreasSwitchvideo28.java
@@ -1,19 +1,24 @@
 package seccion3.ejercicios;
+
 import javax.swing.*;
+
 /**
  *
  * @author Jesús González
  */
 public class CalcularAreasSwitchvideo28 {
-    public static void main(String[] args){
-        
-        int figura= Integer.parseInt(JOptionPane.showInputDialog("Seleccione la figura:"
+
+    public static void main(String[] args) {
+
+        int figura = Integer.parseInt(JOptionPane.showInputDialog("Seleccione la figura:"
                 + "\n1.- Cuadrado"
                 + "\n2.- Rectangulo"
                 + "\n3.- Triangulo"
                 + "\n4.- Circulo\n"));
-        switch(figura){
+        switch (figura) {
             case 1:
+                int lado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el lado:"));
+                JOptionPane.showMessageDialog(null,"El area del Cuadrado es: "+Math.pow(lado, 2));
                 break;
             case 2:
                 break;
@@ -24,7 +29,7 @@ public class CalcularAreasSwitchvideo28 {
             default:
                 break;
         }
-        
+
     }
-    
+
 }
