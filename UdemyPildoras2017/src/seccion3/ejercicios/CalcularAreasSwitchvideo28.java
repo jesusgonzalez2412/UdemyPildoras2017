8b84149b7c33050c7e1b0aca63d19bb9953c26ed
Jesús González <Jess Gonzlez@x0d4y>
2017-10-25 01:55:43 -0500

    Ejercicios Curso Udemy 25-10-17

--- UdemyPildoras2017/src/seccion3/ejercicios/CalcularAreasSwitchvideo28.java
+++ UdemyPildoras2017/src/seccion3/ejercicios/CalcularAreasSwitchvideo28.java
@@ -1,11 +1,17 @@
 package seccion3.ejercicios;
-
+import javax.swing.*;
 /**
  *
  * @author Jesús González
  */
 public class CalcularAreasSwitchvideo28 {
     public static void main(String[] args){
+        
+        int figura= Integer.parseInt(JOptionPane.showInputDialog("Seleccione la figura:"
+                + "\n1.- Cuadrado"
+                + "\n2.- Rectangulo"
+                + "\n3.- Triangulo"
+                + "\n4.- Circulo\n"));
         
     }
     
